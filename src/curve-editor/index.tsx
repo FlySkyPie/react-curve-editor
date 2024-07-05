29468979bf55977f7323e4b744e417f8ba50fa05
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2024-07-05 14:06:38 +0800

    feat: Add line interactive

--- src/curve-editor/index.tsx
+++ src/curve-editor/index.tsx
@@ -1,15 +1,21 @@
 import React, { useMemo, useState, } from 'react'
-import { Canvas, extend } from '@react-three/fiber'
-import { CameraControls, OrthographicCamera, } from '@react-three/drei'
-import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
+import { Canvas, } from '@react-three/fiber'
+import { CameraControls, OrthographicCamera, Line } from '@react-three/drei'
 import useResizeObserver from "use-resize-observer";
 
-extend({ MeshLineGeometry, MeshLineMaterial })
 const PADDING = 0.025;
 
 export const CurveEditor: React.FC = () => {
     const { ref, width = 1, height = 1 } = useResizeObserver<HTMLDivElement>();
     const size = useMemo(() => Math.min(width, height), [height, width]);
+
+    const [hoverPoint, setHoverPoint] = useState<[number, number, number]>();
+    const cursor = useMemo(() => {
+        if (hoverPoint) {
+            return "crosshair"
+        }
+        return undefined;
+    }, [hoverPoint]);
 
     const [positions, setPositions] = useState<[number, number][]>(() => [[0, 0], [1, 1,]]);
     const positionsVec3 = useMemo(() =>
@@ -22,11 +28,33 @@ export const CurveEditor: React.FC = () => {
         </mesh>), [positionsVec3]);
 
     const lineView = useMemo(() =>
-        <mesh >
-            <meshLineGeometry points={positionsVec3.flat()} />
-            <meshLineMaterial lineWidth={0.005} color="#00ff00" />
-        </mesh>
-        , [positionsVec3]);
+        <Line
+            points={positionsVec3 as any}
+            color="#00ff00"
+            lineWidth={3}
+            segments
+            dashed={false}
+        />, [positionsVec3]);
+
+
+    const lineInteractiveView = useMemo(() =>
+        <Line
+            points={positionsVec3 as any}
+            color="#ff00ff"
+            lineWidth={16}
+            segments
+            transparent
+            opacity={0}
+            dashed={false}
+            onPointerMove={({ pointOnLine }) => {
+                if (!pointOnLine) {
+                    return pointOnLine;
+                }
+                const { x, y, z } = pointOnLine;
+                setHoverPoint([x, y, z])
+            }}
+            onPointerLeave={() => setHoverPoint(undefined)}
+        />, [positionsVec3]);
 
     return (
         <div
@@ -43,7 +71,9 @@ export const CurveEditor: React.FC = () => {
                     width: `${size}px`,
                     height: `${size}px`,
                 }}>
-                <Canvas >
+                <Canvas style={{
+                    cursor,
+                }}>
                     <color attach="background" args={["#16161D"]} />
 
                     <ambientLight intensity={Math.PI / 2} />
@@ -52,6 +82,12 @@ export const CurveEditor: React.FC = () => {
 
                     {pointView}
                     {lineView}
+                    {lineInteractiveView}
+                    {hoverPoint &&
+                        <mesh position={hoverPoint}>
+                            <sphereGeometry args={[0.025, 12, 12]} />
+                            <meshBasicMaterial color={0x2da12d} />
+                        </mesh>}
 
                     <OrthographicCamera
                         makeDefault
